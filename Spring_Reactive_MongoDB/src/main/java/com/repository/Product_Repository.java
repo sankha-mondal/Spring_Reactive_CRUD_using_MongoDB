b2560f5a339329483f9a8fb763d78c7f9bfae5bb
sankhamondal400 <sankhasubhra.mondal@wipro.com>
2023-08-11 12:40:48 +0530

    file deleted

--- Spring_Reactive_MongoDB/src/main/java/com/repository/Product_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.entity.Product;
-
-public interface Product_Repository extends ReactiveMongoRepository<Product, String> {
-
-}
